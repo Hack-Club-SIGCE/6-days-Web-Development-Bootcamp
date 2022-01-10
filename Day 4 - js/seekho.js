bacbd4b403ad965b9a9392d243f84eb8aebf822a
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2022-01-10 19:05:32 +0530

    Add files via upload

---
+++ Day 4 - js/seekho.js
@@ -0,0 +1,233 @@
+// console.warn("bhaii bakwas kamm kiya karr");
+// console.error("haa bhaii aage badh");
+// document.write("hjdehgf");
+// variables
+// var a=36*36;
+// document.write(a);
+
+//comments in js - //- single line comment 
+//multi line comment - /* */
+/* mjbdmhjbdjbgjvknbk
+hjbfvmvbfhf
+hvrhdjrbhj
+ngcvfdrjhn
+*/ 
+
+
+// Data types
+// 1. Numbers - 1,2,3.5,8
+// 2. Strings - "siddhi","sobhin",
+// 3. boolean - true or false 
+// 4. undefined - when variable is declared but not given a value
+// 5.null 
+// var a;
+// console.log(a);
+// type of operator
+// typeof null
+// 'object'
+
+//Operators 
+// arithmetic operators 
+// var a=3+5;
+// console.log(a);
+// var a=8-6;
+// console.log(a);
+// var a=3*5;
+// console.log(a);
+// var a=3/5;
+// console.log(a);
+// var a=3%5;
+// console.log(a);
+
+
+// assignment operators
+// var a=35;
+// console.log(a);
+// var a=3;
+// a+=4;
+// console.log(a);
+// var a=3;
+// a-=4;
+// console.log(a);
+// var a=3;
+// a/=4;
+// console.log(a);
+
+//comparison operator 
+//== - value 
+//===- value+datatype
+// var a=5;
+// var b="5";
+// console.log(a==b);
+// console.log(a===b);
+// != !==
+// >= <= > < 
+
+//logical operators
+// && || !
+// 1 bhi false - false - &&
+// 1 bhi true - true - ||
+// ! t-f f-t
+
+// console.log(5>4 && 4<3)
+// console.log(5>4 || 4<3)
+// console.log(! 5>4)
+
+// Conditionals 
+
+var age=2;
+//if-  else statement 
+// if(age>=18){
+// 	console.log("you can drink");
+// }
+// else{
+// 	console.log("kids these days")
+// }
+
+//ternary operator 
+// (age>=18)?console.log("you can drink"):console.log("kids these days");
+
+//if else ladder 
+
+// if(age>=18){
+// 	console.log("adult");
+// }
+// else if(age>=10){
+// 	console.log("teen");
+// }
+// else{
+// 	console.log("kid");
+// }
+ 
+//loops -- repeating 
+// 1 to 10
+//for loop
+// for(var i=1;i<=10;i+=2){
+// 	console.log(i);
+// }
+// i=1
+// i<=10
+// 1
+// i++=2
+// 2<=10
+// 2
+//while loop 
+// var a=1;
+// while(a<=10){
+// 	console.log(a);
+// 	a++;
+// }
+// hw do while loop 
+//var,let const 
+// var-variable
+// let- variable
+// const-constant var - no change 
+// const a=10;
+// // a=23;
+// console.log(a);
+
+//functions 
+// var a=10+8+3+5;
+// var b=10+10+0;
+// var c=
+// function add(a,b){ //fuction definition  a=4 b=8
+// 	let c=a+b; 
+// 	// c=12
+// 	return c; //12 ko return
+
+// }
+// console.log(c); -- let scope
+// let a1=add(4,8); //call invoke 
+// console.log(a1);
+// let a2=add(66,8);
+// console.log(a2);
+
+// function abc(a){
+// 	console.log(a);
+// 	console.log(b);
+// }
+// abc(2);
+//let allows you to declare variables that are limited in scope to the block, statement, or expression on which it is used. This is unlike the var keyword, which defines a variable globally, or locally to an entire function regardless of block scope.
+
+// var a
+// var c 
+// var d 
+// let a=[1,2,3,4,"siddhi",5,6];
+// console.log(a);
+// console.log(a.length);-- imp
+//array methods 
+// a.push(7);
+// console.log(a);
+// a.pop()
+// console.log(a);
+//shift unshift 
+
+
+//strings
+// let a="siddhi";
+// console.log(a.length);
+// console.log(a.slice(0,4));
+// let d=a.replace("sid","didi");
+// console.log(d);
+
+//objects - key value pairs 
+// let student={
+// 	fName:"siddhi",
+// 	lName:"Bhanushali",
+// 	age:21,
+// 	job:"web developer",
+// 	friends:["mrudul","sobhin","rakesh","rutuja"]
+// }
+// console.log(student.lName);
+// console.log(student.friends[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
